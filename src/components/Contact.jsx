77d5033e4256cdb4816c955c1bf78cc0f333dd2a
seand4 <vaseant.davis@gmail.com>
2022-06-16 22:44:07 -0400

    Updated Contact + Footer class to ClassName

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,30 +10,30 @@ const contact = ({
   buttonText,
 }) => {
   return (
-    <section class="contact__wrapper">
-      <div class="contact__header-wrapper">
-        <h1 class="contact__heading">{contactHead}</h1>
-        <p class="contact__text">{contactSub}</p>
+    <section className="contact__wrapper">
+      <div className="contact__header-wrapper">
+        <h1 className="contact__heading">{contactHead}</h1>
+        <p className="contact__text">{contactSub}</p>
       </div>
 
-      <div class="contact__cards-wrapper">
-        <div class="contact__card-wrapper">
-          <i class="fa-solid fa-phone contact__card-image"></i>
-          <p class="contact__card-email">{email1}</p>
-          <h2 class="contact__card-support">{support}</h2>
-          <button class="contact__card-button">{buttonText}</button>
+      <div className="contact__cards-wrapper">
+        <div className="contact__card-wrapper">
+          <i className="fa-solid fa-phone contact__card-image"></i>
+          <p className="contact__card-email">{email1}</p>
+          <h2 className="contact__card-support">{support}</h2>
+          <button className="contact__card-button">{buttonText}</button>
         </div>
-        <div class="contact__card-wrapper middle">
-          <i class="fa-solid fa-location-dot contact__card-image"></i>
-          <p class="contact__card-email">{email2}</p>
-          <h2 class="contact__card-support">{support}</h2>
-          <button class="contact__card-button">{buttonText}</button>
+        <div className="contact__card-wrapper middle">
+          <i className="fa-solid fa-location-dot contact__card-image"></i>
+          <p className="contact__card-email">{email2}</p>
+          <h2 className="contact__card-support">{support}</h2>
+          <button className="contact__card-button">{buttonText}</button>
         </div>
-        <div class="contact__card-wrapper">
-          <i class="fa-solid fa-envelope contact__card-image"></i>
-          <p class="contact__card-email">{email3}</p>
-          <h2 class="contact__card-support">{support}</h2>
-          <button class="contact__card-button">{buttonText}</button>
+        <div className="contact__card-wrapper">
+          <i className="fa-solid fa-envelope contact__card-image"></i>
+          <p className="contact__card-email">{email3}</p>
+          <h2 className="contact__card-support">{support}</h2>
+          <button className="contact__card-button">{buttonText}</button>
         </div>
       </div>
     </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,75 +29,75 @@ const footer = ({
 }) => {
   return (
     <footer>
-      <div class="footer__heading">
-        <h6 class="footer__heading-brand">{head}</h6>
-        <ul class="footer__heading-icons__wrapper">
-          <li class="footer__heading-icon">
-            <i class="fa-brands fa-facebook"></i>
+      <div className="footer__heading">
+        <h6 className="footer__heading-brand">{head}</h6>
+        <ul className="footer__heading-icons__wrapper">
+          <li className="footer__heading-icon">
+            <i className="fa-brands fa-facebook"></i>
           </li>
-          <li class="footer__heading-icon">
-            <i class="fa-brands fa-instagram"></i>
+          <li className="footer__heading-icon">
+            <i className="fa-brands fa-instagram"></i>
           </li>
-          <li class="footer__heading-icon">
-            <i class="fa-brands fa-twitter"></i>
+          <li className="footer__heading-icon">
+            <i className="fa-brands fa-twitter"></i>
           </li>
         </ul>
       </div>
 
-      <hr class="footer__line" />
-      <div class="footer__menu__wrapper">
-        <div class="footer__menu-items__wrapper">
+      <hr className="footer__line" />
+      <div className="footer__menu__wrapper">
+        <div className="footer__menu-items__wrapper">
           <h6>{menu1}</h6>
-          <ul class="footer__menu-lists">
-            <li class="footer__menu-lists__items">{m1item1}</li>
-            <li class="footer__menu-lists__items">{m1item2}</li>
-            <li class="footer__menu-lists__items">{m1item3}</li>
-            <li class="footer__menu-lists__items">{m1item4}</li>
+          <ul className="footer__menu-lists">
+            <li className="footer__menu-lists__items">{m1item1}</li>
+            <li className="footer__menu-lists__items">{m1item2}</li>
+            <li className="footer__menu-lists__items">{m1item3}</li>
+            <li className="footer__menu-lists__items">{m1item4}</li>
           </ul>
         </div>
-        <div class="footer__menu-items__wrapper">
+        <div className="footer__menu-items__wrapper">
           <h6>{menu2}</h6>
-          <ul class="footer__menu-lists">
-            <li class="footer__menu-lists__items">{m2item1}</li>
-            <li class="footer__menu-lists__items">{m2item2}</li>
-            <li class="footer__menu-lists__items">{m2item3}</li>
-            <li class="footer__menu-lists__items">{m2item4}</li>
+          <ul className="footer__menu-lists">
+            <li className="footer__menu-lists__items">{m2item1}</li>
+            <li className="footer__menu-lists__items">{m2item2}</li>
+            <li className="footer__menu-lists__items">{m2item3}</li>
+            <li className="footer__menu-lists__items">{m2item4}</li>
           </ul>
         </div>
-        <div class="footer__menu-items__wrapper">
+        <div className="footer__menu-items__wrapper">
           <h6>{menu3}</h6>
-          <ul class="footer__menu-lists">
-            <li class="footer__menu-lists__items">{m3item1}</li>
-            <li class="footer__menu-lists__items">{m3item2}</li>
-            <li class="footer__menu-lists__items">{m3item3}</li>
-            <li class="footer__menu-lists__items">{m3item4}</li>
+          <ul className="footer__menu-lists">
+            <li className="footer__menu-lists__items">{m3item1}</li>
+            <li className="footer__menu-lists__items">{m3item2}</li>
+            <li className="footer__menu-lists__items">{m3item3}</li>
+            <li className="footer__menu-lists__items">{m3item4}</li>
           </ul>
         </div>
-        <div class="footer__menu-items__wrapper">
+        <div className="footer__menu-items__wrapper">
           <h6>{menu4}</h6>
-          <ul class="footer__menu-lists">
-            <li class="footer__menu-lists__items">{m4item1}</li>
-            <li class="footer__menu-lists__items">{m4item2}</li>
-            <li class="footer__menu-lists__items">{m4item3}</li>
-            <li class="footer__menu-lists__items">{m4item4}</li>
+          <ul className="footer__menu-lists">
+            <li className="footer__menu-lists__items">{m4item1}</li>
+            <li className="footer__menu-lists__items">{m4item2}</li>
+            <li className="footer__menu-lists__items">{m4item3}</li>
+            <li className="footer__menu-lists__items">{m4item4}</li>
           </ul>
         </div>
-        <div class="footer__menu-items__wrapper">
+        <div className="footer__menu-items__wrapper">
           <h6>{menu5}</h6>
-          <form class="footer__menu-form">
+          <form className="footer__menu-form">
             <input
               type="email"
               placeholder="Your Email"
-              class="footer__menu-form__input"
+              className="footer__menu-form__input"
             />
-            <button type="button" class="footer__menu-form__button">
+            <button type="button" className="footer__menu-form__button">
               {buttonText}
             </button>
           </form>
           <p>{formSub}</p>
         </div>
       </div>
-      <p class="disclaimer">{disclaimer}</p>
+      <p className="disclaimer">{disclaimer}</p>
     </footer>
   );
 };
