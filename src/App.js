39c63342e1a6298df72a8bc4319330acdfecc5ec
seand4 <vaseant.davis@gmail.com>
2022-06-18 13:08:07 -0400

    Added Js for mobile nav & video

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -12,6 +12,53 @@ import "./reset.css";
 import "./styles.css";
 
 const App = () => {
+  useEffect(() => {
+    var header = Array.from(document.querySelectorAll("header"));
+    var mobileNavButton = document.querySelector(
+      "div.mobile-header__icon-wrapper i.fa-solid.fa-bars-staggered"
+    );
+    var mobileHeaderNav = document.querySelector(
+      "nav.mobile-header__menu-items__wrapper"
+    );
+
+    var video = document.querySelector("video");
+    var playButton = document.querySelector("i.fa-solid.fa-play");
+    var pauseButton = document.querySelector("i.fa-solid.fa-pause");
+
+    window.addEventListener("scroll", function (e) {
+      let currentScroll = window.pageYOffset;
+      if (currentScroll >= 46) {
+        header.forEach((element) => {
+          element.style.backgroundColor = "rgba(37, 43, 66, .9)";
+        });
+      }
+      // console.log("this is our event target", window.pageYOffset, currentScroll);
+      // console.log("this is main.js !!!!", header);
+    });
+
+    mobileNavButton.addEventListener("click", (e) => {
+      mobileHeaderNav.classList.toggle("flexed");
+      console.log("display", mobileHeaderNav.classList);
+    });
+
+    playButton.addEventListener("click", (e) => {
+      e.target.style.display = "none";
+      pauseButton.style.display = "flex";
+      pauseButton.style.backgroundColor = "transparent";
+      video.play();
+      console.log("play target", e.target);
+    });
+    pauseButton.addEventListener("click", (e) => {
+      e.target.style.display = "none";
+      playButton.style.display = "flex";
+      video.load();
+      console.log("pause target", e.target);
+    });
+
+    console.log("button", playButton);
+    console.log("video", video);
+  }, []);
+
   return (
     <main className="container">
       <Header
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const header = ({logo, item1, item2, item3, item4, heroButton, login}) => {
-    return (
-        <header className="header__wrapper">
-        <div className="header__logo">{logo}</div>
-        <nav className="header__menu-items__wrapper">
-          <ul className="header__menu-items">
-            <li className="header__menu-item">{item1}</li>
-            <li className="header__menu-item">{item2}</li>
-            <li className="header__menu-item">{item3}</li>
-            <li className="header__menu-item">{item4}</li>
-          </ul>
-        </nav>
-        <div className="header__button-wrapper">
-          <a href="#" className="header__button-login">{login}</a>
-          <button className="header__button-member">{heroButton}</button>
-        </div>
-      </header>
-    )
-}
+const header = ({ logo, item1, item2, item3, item4, heroButton, login }) => {
+  return (
+    <div>
+    <header className="header__wrapper">
+      <div className="header__logo">{logo}</div>
+      <nav className="header__menu-items__wrapper">
+        <ul className="header__menu-items">
+          <li className="header__menu-item">{item1}</li>
+          <li className="header__menu-item">{item2}</li>
+          <li className="header__menu-item">{item3}</li>
+          <li className="header__menu-item">{item4}</li>
+        </ul>
+      </nav>
+      <div className="header__button-wrapper">
+        <a href="#" className="header__button-login">
+          {login}
+        </a>
+        <button className="header__button-member">{heroButton}</button>
+      </div>
+    </header>
 
-export default header
+     <header className="mobile-header">
+     <div className="mobile-header__wrapper">
+       <div className="mobile-header__logo">{logo}</div>
+       <div className="mobile-header__icon-wrapper">
+         <i className="fa-solid fa-magnifying-glass"></i>
+         <i className="fa-solid fa-cart-shopping"></i>
+         <i className="fa-solid fa-bars-staggered"></i>
+       </div>
+     </div>
+
+     <nav className="mobile-header__menu-items__wrapper">
+       <ul className="mobile-header__menu-items">
+         <li className="mobile-header__menu-item">{item1}</li>
+         <li className="mobile-header__menu-item">{item2}</li>
+         <li className="mobile-header__menu-item">{item3}</li>
+         <li className="mobile-header__menu-item">{item4}</li>
+       </ul>
+     </nav>
+   </header>
+    </div>
+  );
+};
+
+export default header;
